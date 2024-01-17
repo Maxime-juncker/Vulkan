01419d1743d864054ef95df3dd1ff6e9d18870b2
Juncker Maxime <82824938+Maxime-juncker@users.noreply.github.com>
2024-01-17 18:04:30 +0100

    la pizza de la mama EN PLUS GROS

--- Source/Private/Model.cpp
+++ Source/Private/Model.cpp
@@ -441,7 +441,7 @@ namespace Application
 		// Note : glm::rotate(glm::mat4(1.0f) = matrice identity 4
 		ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(-90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 		ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-		ubo.proj = glm::perspective(glm::radians(45.f), 
+		ubo.proj = glm::perspective(glm::radians(35.f), // FOV
 			swapChain.GetSwapChainExtent().width / (float)swapChain.GetSwapChainExtent().height, 0.1f, 10.0f);
 		ubo.proj[1][1] *= -1; // GLM is design for opengl but the y axis is inverted in vulkan so we flip it.
 
